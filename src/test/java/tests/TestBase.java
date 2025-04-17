a414be9ef3439f0df8e3837df89fe566c999720f
YanaTsiFi <yanagrischechko@gmail.com>
2025-04-17 23:16:28 +0300

    added tests

--- src/test/java/tests/TestBase.java
+++ src/test/java/tests/TestBase.java
@@ -1,23 +1,59 @@
 package tests;
 
 import com.codeborne.selenide.Configuration;
+import com.codeborne.selenide.WebDriverRunner;
+import com.codeborne.selenide.logevents.SelenideLogger;
+import helpers.Attach;
+import io.qameta.allure.selenide.AllureSelenide;
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
+import org.openqa.selenium.remote.DesiredCapabilities;
+
+import java.util.Map;
 
 import static com.codeborne.selenide.Selenide.*;
 
 public class TestBase {
 
     @BeforeAll
-    static void setUp() {
+    static void beforeAll() {
         Configuration.baseUrl = "https://tigsipay.com/en";
         Configuration.browser = System.getProperty("browser", "chrome");
+        Configuration.browserVersion = System.getProperty("browserVersion", "100.0");
         Configuration.browserSize = System.getProperty("browserSize", "1920x1080");
-        Configuration.timeout = 5000;
+        Configuration.remote = System.getProperty("remoteUrl");
+        Configuration.timeout = 10000;
+
+        if (Configuration.remote != null) {
+            DesiredCapabilities capabilities = new DesiredCapabilities();
+            capabilities.setCapability("selenoid:options", Map.of(
+                    "enableVNC", true,
+                    "enableVideo", true
+            ));
+            Configuration.browserCapabilities = capabilities;
+        }
+
+        SelenideLogger.addListener("AllureSelenide", new AllureSelenide()
+                .screenshots(true)
+                .savePageSource(true));
     }
 
     @BeforeEach
     void openMainPage() {
         open("/");
+    }
+
+    @AfterEach
+    void addAttachments() {
+        Attach.screenshotAs("Screenshot");
+        Attach.pageSource();
+        Attach.browserConsoleLogs();
+
+        if (Configuration.remote != null) {
+            Attach.addVideo();
+        }
+
+        WebDriverRunner.closeWebDriver();
     }
 }
